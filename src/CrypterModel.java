e9caa75fbf44069d2c59f2ef788c47510fef1a50
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-10-14 12:28:19 +0200

    Crypt method

--- src/CrypterModel.java
+++ src/CrypterModel.java
@@ -4,19 +4,30 @@ public class CrypterModel {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("Write your string");
         String key = JOptionPane.showInputDialog("Enter a key");
+        String output = Encrypt(input, key);
+        JOptionPane.showMessageDialog(null,output);
+        System.out.println(output);
+
+    }
+
+    private static String Encrypt(String input, String key) {
         int x;
         int y;
         String output = "";
 
-        for (int i = 0; i<input.length(); i++){
+        for (int i = 0; i< input.length(); i++){
             x =  input.charAt(i);
             y =  key.charAt(i);
-            int z = x^y;
+            int z = crypt(x, y);
             char c = (char)z;
             output += c;
         }
-        System.out.println(output);
+        return output;
+    }
 
+    private static int crypt(int x, int y) {
+        int z = x ^ y;
+        return z;
     }
 
 }
